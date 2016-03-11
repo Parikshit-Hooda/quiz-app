31c68cbb770c8efd811146ad96899005681ac3c3
Rico Chen <ricochenx@gmail.com>
2016-03-10 23:06:13 -0800

    added correctAnswer function and choices

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function ready(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
 function changeChoices() {
 	var questions = [
 	{question: 'What is 1+1?',
@@ -7,10 +15,22 @@ function changeChoices() {
 	choices:[2, 3, 4, 5],
 	correct:2}
 ];
-	return function () {
-		for (var i = 0; i < document.answers.choice.length; i++) {
-			document.getElementById('choice1').innerHTML = questions[0]['choices'][i];
-		}
+	document.getElementById('questionNum').innerHTML += 1;
+
+	document.getElementById('question').innerHTML = questions[0]['question'];
+	
+	document.getElementById('choice1').innerHTML = questions[0]['choices'][0];
+	document.getElementById('choice2').innerHTML = questions[0]['choices'][1];
+	document.getElementById('choice3').innerHTML = questions[0]['choices'][2];
+	document.getElementById('choice4').innerHTML = questions[0]['choices'][3];
+}
+
+function correctAnswer() {
+	if (document.answers.choice2.checked === true) {
+		alert('correct');
+	} else {
+		alert('wrong');	
 	}
 }
 
+ready(changeChoices);
