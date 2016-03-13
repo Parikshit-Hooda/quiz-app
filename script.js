72708aa2f0952c06e063090679f11019a9a7f012
Rico Chen <ricochenx@gmail.com>
2016-03-12 16:41:56 -0800

    changed questions and styled page

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* function ready(fn) {
+function ready(fn) {
   if (document.readyState != 'loading'){
     fn();
   } else {
@@ -8,15 +8,13 @@
 
 ready(choices);
 
-*/
-
 var content = 
 [
-	{question: 'What is 1+1?', choices: [1, 2, 3, 4], correct: 1},
-	{question: 'What is 2+2?', choices: [2, 3, 4, 5], correct: 2},
-	{question: 'What is 2+3?', choices: [2, 's', 4, 5], correct: 2},
-	{question: 'What is 2+4?', choices: [2, 's', 4, 5], correct: 2},
-	{question: 'What is 2+5?', choices: [2, 3, 'z', 5], correct: 2},
+	{question: 'How many months have 28 days?', choices: [1, 6, 8, 12], correct: 3},
+	{question: 'What is the missing number: 4, 8, 14, 22, ?', choices: [26, 28, 32, 36], correct: 2},
+	{question: 'There are 12 pens on the table, you took 3, how many do you have?', choices: [3, 12, 9, 15], correct: 0},
+	{question: 'Solve this equation using an algebraic method: (x + 4)( x - 4) = 9', choices: ['x = 5', 'x = 5 and x = -5', 'x = 9', 'x = 4 and x= -4'], correct: 1},
+	{question: 'Which of the words below is spelled incorrectly?', choices: ['Inoculate', 'Consensus', 'Liquefy', 'Sacreligious'], correct: 3},
 ];
 
 var x = 0;
@@ -24,14 +22,12 @@ var y = 1;
 var score = 0;
 
 function choices() {
-
 	if (content[x] === undefined) {
 		document.querySelector('.score').textContent = 'Score: ' + score;
 		document.form1.style.visibility = 'hidden';
 		document.querySelector('.header').style.visibility = 'hidden';
 
 	} else {
-
 		var questionNumber = document.querySelector('.questionNumber');
 		questionNumber.textContent = 'Question#' + y;
 
@@ -66,6 +62,5 @@ function next() {
 		x++;
 		y++;
 		choices();
-
 	}
 }
